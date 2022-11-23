f00b023b19285e190d0954e67489ac834246c4bc
Bryan <bryanstevenal@gmail.com>
2022-11-23 20:30:55 +0100

    Use gifsService to storage all user's searchs

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
     selector: 'app-busqueda',
@@ -10,11 +11,18 @@ export class BusquedaComponent {
     // el objeto no será nulo https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-0.html#non-null-assertion-operator
     @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
+    constructor(
+        private gifsService: GifsService
+    ) {
+
+    }
     buscar() {
         const valor = this.txtBuscar.nativeElement.value;
         console.log("[GifsModule] [BusquedaComponent] [buscar()] Buscar: ", valor);
 
         this.txtBuscar.nativeElement.value = '';
+
+        this.gifsService.buscarGifs(valor);
     }
 
 }
