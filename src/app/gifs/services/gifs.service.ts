e522f052f7709a40d375693c21580ce1048bad98
Bryan <bryanstevenal@gmail.com>
2022-11-27 12:44:24 +0100

    Use HttpParams in order to set Query params

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Gif, SearchGifsResponse } from '../Interface/gifs.interface';
 
 @Injectable({
@@ -12,6 +12,7 @@ import { Gif, SearchGifsResponse } from '../Interface/gifs.interface';
 export class GifsService {
 
     private apiKey: string = 'a7Qs9SjSPrDATTFxDfrNGjKmoG9VA6Zg';
+    private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
     private _historial: string[] = [];
 
     public resultados: Gif[] = [];
@@ -54,8 +55,15 @@ export class GifsService {
 
         console.log('[GifsService] [buscarGifs()] Current historial: ', this._historial);
 
+        const params = new HttpParams()
+            .set("api_key", this.apiKey)
+            .set("limit", "10")
+            .set("q", query);
+
+        console.log('[GifsService] [buscarGifs()] Params: ', params);
+
         // Obtener los gifs usando HttpClientModule
-        this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?q=${query}&limit=10&api_key=a7Qs9SjSPrDATTFxDfrNGjKmoG9VA6Zg`)
+        this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`, { params })
             .subscribe((response) => {
                 console.log(`[GifsService][buscarGifs()] Enpoint SEARCH "${query}" response: `, response.data);
                 this.resultados = response.data;
